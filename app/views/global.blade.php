06837b6a1b66bbc3df8d1d38a4a924139371a840
56347andy <washington_andy@hotmail.com>
2013-10-29 01:55:33 -0700

    Ported site over to framework

---
+++ app/views/global.blade.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+
+<link rel="stylesheet" href="css/main.css" />
+</head>
+<body>
+<div id="container">
+<div id="header">
+<nav id="required">
+
+</nav>
+</div>
+<div id="main">
+<div id="nav"><ul>
+<li><a href="#">Academics</a>
+<ul>
+<li><a href="#">Accreditation</a></li>
+<li><a href="#">AP Information</a></li>
+<li><a href="#">Avid</a></li>
+<li><a href="#">Curriculum Guide</a></li>
+<li><a href="#">Graduation Requirements</a></li>
+<li><a href="#">Senior Info</a></li>
+<li><a href="#">SMT Magnet</a></li>
+<li><a href="#">State Testing</a></li>
+<li><a href="#">VPS Performance Report</a></li>
+</ul>
+</li>
+<li><a href="#">Activities</a>
+<ul>
+<li><a href="#">ASB</a></li>
+<li><a href="#">Clubs</a></li>
+</ul>
+</li>
+<li><a href="#">Career Center</a>
+<ul>
+<li><a href="#">Career Center Home</a></li>
+<li><a href="#">Career Crusing</a></li>
+</ul>
+</li>
+<li><a href="#">Counseling Center</a>
+<ul>
+<li><a href="#">Counseling Center Home</a></li>
+<li><a href="#">Running Start</a></li>
+</ul>
+</li>
+<li><a href="#">Faculty</a>
+<ul>
+<li><a href="#">Administration</a></li>
+</ul>
+</li>
+<li><a href="#">Library</a>
+<ul>
+<li><a href="#"></a></li>
+</ul>
+</li>
+<li><a href="#">Skyview Information</a>
+<ul>
+<li><a href="#">Bell 	</a></li>
+</ul>
+</li>
+<li><a href="#">Sports</a>
+<ul>
+<li><a href="#"></a></li>
+</ul>
+</li>
+<li><a href="#">Student Services</a>
+<ul>
+<li><a href="#"></a></li>
+</ul>
+</li>
+</ul></div>
+<div id="content">
+@yield('content')
+</div>
+</div>
+<div id="footer">
+
+</div>
+</div>
+<script src="js/jquery.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -1,39 +1,5 @@
 @extends('global')
 
-<section>
-                        <table>
-                                <tr>
-                                        <td id="announce">
-                                                <h3>Daily Announcements</h3>
-                                                <ul>
-                                                        <li>Red Cross meeting today after school in the 600 wing.</li>
-                                                        <li>If you are interested in trying out for the boys basketball program this season, there will be an informational meeting on Tuesday, October 22nd at 2:15 PM in the auditorium.</li>
-                                                        <li>Knowledge bowl will meet tonight at 6 PM in the 600 wing. Much fun will be had. One of the questions tonight will be ... In 1699, the first settlement in the region that became what state was at Biloxi? Bring your answer tonight and you'll win a prize.</li>
-                                                        <li>IQ won't we open tomorrow but we will be open the rest of the week so make sure to stop by and participate in our October promotion!</li>
-                                                        <li>All girl swimmers if you need a picture packet for team pictures come pick up a packet in Mr. Yakjo room 802 D any time today.</li>
-                                                        <li>German club meeting today @ 2:15 in Mrs. Johnson's room 502 C.</li>
-                                                        <li>On Friday the boys cross country teams ran well at the Finn Hill Invite. Our JV team placed 4th overall and Jacob Gillingham placing 10th. In the varsity race the Storm runners placed 9th with Grant Lindberg leading the team.</li>
-                                                        <li>All girls interested in joining the Skyview bowling team this year, please come to room 403 after school Wednesday at 12:15. Bowling team is an awesome, fun way to be a part of the Skyview athletics. Beginners welcome.</li>
-                                                        <li>Exorcisms in the 400 wing after school from 2:15 to 3:30</li>
-                                                        <li>Sign up in the Career Center to take the ASVAB- The Armed Services Vocational Aptitude Battery test on Saturday November 9th at Hudson's Bay High School. The test is open to juniors and seniors. No registration fee. Taking the ASVAB is a great way to help you make career and post high school educational plans.</li>
-                                                        <li>Attention gear up students! There is an exciting new creative media and design program opportunity for freshmen call the BIZ program. Have you ever wanted to make a music video or create an online magazine? Then join us for an informational meeting today and tomorrow during tutorial in the downstairs wrestling room to learn more about it and apply.</li>
-                                                        <li>Red Cross meeting today after school in the 600 wing.</li>
-                                                        <li>If you are interested in trying out for the boys basketball program this season, there will be an informational meeting on Tuesday, October 22nd at 2:15 PM in the auditorium.</li>
-                                                        <li>Knowledge bowl will meet tonight at 6 PM in the 600 wing. Much fun will be had. One of the questions tonight will be ... In 1699, the first settlement in the region that became what state was at Biloxi? Bring your answer tonight and you'll win a prize.</li>
-                                                        <li>IQ won't we open tomorrow but we will be open the rest of the week so make sure to stop by and participate in our October promotion!</li>
-                                                        <li>All girl swimmers if you need a picture packet for team pictures come pick up a packet in Mr. Yakjo room 802 D any time today.</li>
-                                                        <li>German club meeting today @ 2:15 in Mrs. Johnson's room 502 C.</li>
-                                                        <li>On Friday the boys cross country teams ran well at the Finn Hill Invite. Our JV team placed 4th overall and Jacob Gillingham placing 10th. In the varsity race the Storm runners placed 9th with Grant Lindberg leading the team.</li>
-                                                        <li>All girls interested in joining the Skyview bowling team this year, please come to room 403 after school Wednesday at 12:15. Bowling team is an awesome, fun way to be a part of the Skyview athletics. Beginners welcome.</li>
-                                                        <li>Sign up in the Career Center to take the ASVAB- The Armed Services Vocational Aptitude Battery test on Saturday November 9th at Hudson's Bay High School. The test is open to juniors and seniors. No registration fee. Taking the ASVAB is a great way to help you make career and post high school educational plans.</li>
-                                                        <li>Attention gear up students! There is an exciting new creative media and design program opportunity for freshmen call the BIZ program. Have you ever wanted to make a music video or create an online magazine? Then join us for an informational meeting today and tomorrow during tutorial in the downstairs wrestling room to learn more about it and apply.</li>
-                                                </ul>
-                                        </td>
-                                        <td id="info">
-                                                <h3>Information</h3>
-                                        </td>
-                                </tr>
-                        </table>
-                </section>
+@section('content')
 
 @stop
