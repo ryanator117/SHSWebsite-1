cf777dc9d41d3814c36678705acce73c4fcff3cc
56347andy <washington_andy@hotmail.com>
2013-10-31 00:10:23 -0700

    Commit

--- app/views/global.blade.php
+++ app/views/global.blade.php
@@ -16,106 +16,101 @@
 <div id="nav"><ul>
 	<li><a href="#">Academics</a>
 		<ul>
-		<li><a href="#">Accreditation</a></li>
-		<li><a href="#">AP Information</a></li>
-		<li><a href="#">Avid</a></li>
-		<li><a href="#">Curriculum Guide</a></li>
-		<li><a href="#">Graduation Requirements</a></li>
-		<li><a href="#">Senior Info</a></li>
-		<li><a href="#">SMT Magnet</a></li>
-		<li><a href="#">State Testing</a></li>
-		<li><a href="#">VPS Performance Report</a></li>
+			<li><a href="#">Accreditation</a></li>
+			<li><a href="#">AP Information</a></li>
+			<li><a href="#">Avid</a></li>
+			<li><a href="#">Curriculum Guide</a></li>
+			<li><a href="#">Graduation Requirements</a></li>
+			<li><a href="#">Senior Info</a></li>
+			<li><a href="#">SMT Magnet</a></li>
+			<li><a href="#">State Testing</a></li>
+			<li><a href="#">VPS Performance Report</a></li>
 		</ul>
 	</li>
 	<li><a href="#">Activities</a>
 		<ul>
-		<li><a href="#">ASB</a></li>
-		<li><a href="#">Clubs</a></li>
+			<li><a href="#">ASB</a></li>
+			<li><a href="#">Clubs</a></li>
 		</ul>
 	</li>
 	<li><a href="#">Career Center</a>
 		<ul>
-		<li><a href="#">Career Center Home</a></li>
-		<li><a href="#">Career Crusing</a></li>
+			<li><a href="#">Career Center Home</a></li>
+			<li><a href="#">Career Crusing</a></li>
 		</ul>
 	</li>
 	<li><a href="#">Counseling Center</a>
 		<ul>
-		<li><a href="#">Counseling Center Home</a></li>
-		<li><a href="#">Running Start</a></li>
+			<li><a href="#">Counseling Center Home</a></li>
+			<li><a href="#">Running Start</a></li>
 		</ul>
 	</li>
 	<li><a href="#">Faculty</a>
 		<ul>
-		<li><a href="#">Administration</a></li>
-		<li><a href="#">Counselers</a></li>
-		<li><a href="#">Support Staff</a></li>
-		<li><a href="#">Teachers</a></li>
+			<li><a href="#">Administration</a></li>
+			<li><a href="#">Counselers</a></li>
+			<li><a href="#">Support Staff</a></li>
+			<li><a href="#">Teachers</a></li>
 		</ul>
 	</li>
 	<li><a href="#">Library</a>
 		<ul>
-		<li><a href="#">Library Home</a></li>
-		<li><a href="#">Catalog</a></li>
+			<li><a href="#">Library Home</a></li>
+			<li><a href="#">Catalog</a></li>
 		</ul>
 	</li>
 	<li><a href="#">Skyview Information</a>
 		<ul>
-		<li><a href="#">Bell Schedule</a></li>
-		<li><a href="#">Business Office</a></li>
-		<li><a href="#">Emergency School Closures</a></li>
-		<li><a href="#">Mission Statement</a></li>
-		<li><a href="#">Senior Information</a></li>
-		<li><a href="#">Student Handbook</a></li>
-		<li><a href="#">Technology Policy</a></li>
+			<li><a href="#">Bell Schedule</a></li>
+			<li><a href="#">Business Office</a></li>
+			<li><a href="#">Emergency School Closures</a></li>
+			<li><a href="#">Mission Statement</a></li>
+			<li><a href="#">Senior Information</a></li>
+			<li><a href="#">Student Handbook</a></li>
+			<li><a href="#">Technology Policy</a></li>
 		</ul>
 	</li>
 	<li><a href="#">Sports</a>
 		<ul>
-		<li><a href="#">Fall</a>
-			<ul>
-			<li><a href="#">Football</a></li>
-			<li><a href="#">Volleyball</a></li>
-			<li><a href="#">Cross Country Boys</a></li>
-			<li><a href="#">Cross Country Girls</a></li>
-			<li><a href="#">Girls Soccer</a></li>
-			<li><a href="#">Boys Tennis</a></li>
-			<li><a href="#">Boys Tennis</a></li>
-			<li><a href="#">Boys Golf</a></li>
-			<li><a href="#">Girls Swimming</a></li>
-			<li><a href="#">Cheer</a></li>
-			<li><a href="#">Color Guard</a></li>
-			</ul>
-		</li>
-		<li><a href="#">Winter</a>
-			<ul>
-			<li><a href="#">Boys Basketball</a></li>
-			<li><a href="#">Girls Basketball</a></li>
-			<li><a href="#">Wrestling</a></li>
-			<li><a href="#">Gymnastics</a></li>
-			<li><a href="#">Boys Swimming</a></li>
-			<li><a href="#">Cheer</a></li>
-			<li><a href="#">Color Guard</a></li>
-			<li><a href="#">Bowling</a></li>
-			</ul>
-		</li>
-		<li><a href="#">Spring</a>
-			<ul>
-				<li><a href="#">Baseball</a></li>
-				<li><a href="#">Softball</a></li>
-				<li><a href="#">Track Girls</a></li>
-				<li><a href="#">Track Boys</a></li>
-				<li><a href="#">Boys Soccer</a></li>
-				<li><a href="#">Girls Tennis</a></li>
-				<li><a href="#">Girls Golf</a></li>
-			</ul>
-		</li>
+			<li><a href="#">Fall</a>
+				<ul>
+				<li><a href="#">Football</a></li>
+				<li><a href="#">Volleyball</a></li>
+				<li><a href="#">Cross Country Boys</a></li>
+				<li><a href="#">Cross Country Girls</a></li>
+				<li><a href="#">Girls Soccer</a></li>
+				<li><a href="#">Boys Tennis</a></li>
+				<li><a href="#">Boys Tennis</a></li>
+				<li><a href="#">Boys Golf</a></li>
+				<li><a href="#">Girls Swimming</a></li>
+				<li><a href="#">Cheer</a></li>
+				<li><a href="#">Color Guard</a></li>
+				</ul>
+			</li>
+			<li><a href="#">Winter</a>
+				<ul>
+				<li><a href="#">Boys Basketball</a></li>
+				<li><a href="#">Girls Basketball</a></li>
+				<li><a href="#">Wrestling</a></li>
+				<li><a href="#">Gymnastics</a></li>
+				<li><a href="#">Boys Swimming</a></li>
+				<li><a href="#">Cheer</a></li>
+				<li><a href="#">Color Guard</a></li>
+				<li><a href="#">Bowling</a></li>
+				</ul>
+			</li>
+			<li><a href="#">Spring</a>
+				<ul>
+					<li><a href="#">Baseball</a></li>
+					<li><a href="#">Softball</a></li>
+					<li><a href="#">Track Girls</a></li>
+					<li><a href="#">Track Boys</a></li>
+					<li><a href="#">Boys Soccer</a></li>
+					<li><a href="#">Girls Tennis</a></li>
+					<li><a href="#">Girls Golf</a></li>
+				</ul>
+			</li>
 		</ul>
-		<li><a href="#">Activities</a>
-			<ul>
-				<li><a href="#">Band</a></li>
-			</ul>
-		</li>
 	</li>
 	<li><a href="#">Student Services</a>
 		<ul>
